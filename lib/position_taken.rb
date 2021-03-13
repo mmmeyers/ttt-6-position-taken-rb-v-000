b913f61400b6534d5acfef5d5fe3faae45dfac9e
Marsi Meyers-Miyashita <marsi286@gmail.com>
2021-03-13 03:12:00 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,11 @@
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == ""
+    false
+  elsif board.include?(nil)
+    false
+  else
+    true 
+  end
+
+end
 # code your #position_taken? method here!
